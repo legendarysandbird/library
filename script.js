28798501acd5a54a8c5a09fe91ffed4c5877992b
Brandon Dodd <legendarysandbird@gmail.com>
2021-09-04 07:28:22 -0700

    Edited to use classes

--- script.js
+++ script.js
@@ -1,64 +1,72 @@
-let myLibrary = [];
+class Library {
+    constructor() {
+        this.myLibrary = [];
+    }
 
-function Book(author, title, pages) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = false;
-}
+    addBookToLibrary() {
+        let title = window.prompt("Title: ");
+        let author = window.prompt("Author: ");
+        let pages = window.prompt("Number of pages: ");
 
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
-}
+        let b1 = new Book(author, title, pages);
+        this.myLibrary.push(b1);
+        this.displayLibrary();
+    }
 
-function addBookToLibrary() {
-    let title = window.prompt("Title: ");
-    let author = window.prompt("Author: ");
-    let pages = window.prompt("Number of pages: ");
+    displayLibrary() {
+        let bookshelf = document.getElementById("bookshelf");
+        bookshelf.innerHTML = "";
 
-    let b1 = new Book(author, title, pages);
-    myLibrary.push(b1);
-    displayLibrary();
-}
+        this.myLibrary.forEach((book) => {
 
-function displayLibrary() {
-    let bookshelf = document.getElementById("bookshelf");
-    bookshelf.innerHTML = "";
+            // The book text
+            let newBook = document.createElement("div");
+            newBook.id = "book";
+            newBook.textContent = `${book.title} by ${book.author} is ${book.pages} pages.`
 
-    myLibrary.forEach((book) => {
+            // The read button
+            let read = document.createElement("input");
+            read.type = "checkbox";
+            read.id = "read";
+            read.addEventListener("change", () => {
+                book.toggleRead();
+            })
+            newBook.appendChild(read);
 
-        // The book text
-        let newBook = document.createElement("div");
-        newBook.id = "book";
-        newBook.textContent = `${book.title} by ${book.author} is ${book.pages} pages.`
-
-        // The read button
-        let read = document.createElement("input");
-        read.type = "checkbox";
-        read.id = "read";
-        read.addEventListener("change", () => {
-            book.toggleRead();
-        })
-        newBook.appendChild(read);
-
-        // The remove button
-        let remove = document.createElement("button");
-        remove.id = "remove";
-        remove.textContent = "Remove";
-        remove.addEventListener("click", () => {
-            bookshelf.removeChild(newBook);
-            let index = myLibrary.indexOf((book));
-            myLibrary.splice(index, 1);
-        })
-        newBook.appendChild(remove);
+            // The remove button
+            let remove = document.createElement("button");
+            remove.id = "remove";
+            remove.textContent = "Remove";
+            remove.addEventListener("click", () => {
+                bookshelf.removeChild(newBook);
+                let index = this.myLibrary.indexOf((book));
+                this.myLibrary.splice(index, 1);
+            })
+            newBook.appendChild(remove);
         
-        // Add the book
-        bookshelf.appendChild(newBook);
-    });
+            // Add the book
+            bookshelf.appendChild(newBook);
+        });
         
+    }
 }
+
+class Book {
+    constructor(author, title, pages) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = false;
+    }
+
+    toggleRead() {
+        this.read = !this.read;
+    }
+}
+
+l1 = new Library();
 
 let newButton = document.getElementById("new");
 newButton.addEventListener("click", () => {
-    addBookToLibrary();
+    l1.addBookToLibrary();
 })
